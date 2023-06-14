5e9520ebb1c133ff150273bd1ead93d9910ac5c0
TongYang-tech <tongyang80@yahoo.com>
2023-06-14 16:02:27 -0500

    adding logic with getComputerChoice and playRound function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 
+function getComputerChoice(){
+    const computerChoice = ["rock", "paper", "scissors"];
+    const indexNum = Math.floor(Math.random() * computerChoice.length);
+    return computerChoice[indexNum];
+}
 
-const name = 'Tong';
-const greetings = `hello, ${name}`;
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return "Draw";
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() === "paper"){
+        return "Computer wins";
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() === "scissors"){
+        return "Computer wins";
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() === "paper"){
+        return "Computer wins";
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() === "rock"){
+        return "Player wins";
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() === "scissors"){
+        return "Player wins";
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() === "paper"){
+        return "Player wins";
+    } else {
+        return "Error";
+    }
+  }
 
-console.log(greetings);
-
-(function() {
-    console.log(greetings);
-});
+function game() {
+    
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
