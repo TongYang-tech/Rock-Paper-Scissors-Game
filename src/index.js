071c2760bad11341b55cb60c41f61204e5bdecdb
TongYang-tech <tongyang80@yahoo.com>
2023-06-16 15:24:10 -0500

    added a working functional button. Added a scoreboard, player/computer score, and Title of game.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,105 @@
 
-function getComputerChoice(){
+// Tracking Scores and the winner of that round
+let playerScore = 0;
+let computerScore = 0;
+let roundStatus = "";
+
+// UI buttons users to interact with
+const rockButton = document.querySelector('#rbtn');
+const paperButton = document.querySelector('#pbtn');
+const scissorsButton = document.querySelector('#sbtn');
+
+// Scoredboard
+const winnerStatus = document.querySelector('#status');
+const player = document.querySelector('#player-Score');
+const computer = document.querySelector('#computer-Score');
+
+function getComputerChoice() {
     const computerChoice = ["rock", "paper", "scissors"];
     const indexNum = Math.floor(Math.random() * computerChoice.length);
     return computerChoice[indexNum];
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        return "Draw";
-    } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() === "paper"){
-        return "Computer wins";
-    } else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() === "scissors"){
-        return "Computer wins";
-    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() === "paper"){
-        return "Computer wins";
-    } else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() === "rock"){
-        return "Player wins";
-    } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() === "scissors"){
-        return "Player wins";
-    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() === "paper"){
-        return "Player wins";
-    } else {
-        return "Error";
-    }
-  }
-
-function game() {
-    
+function isGameFinished() {
+    return playerScore == 2 || computerScore == 2; 
 }
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+function playRound(playerSelection, computerSelection) {
+    pSelect = playerSelection.toLowerCase();
+    cSelect = computerSelection.toLowerCase();
+    if (pSelect === cSelect) {
+        roundStatus = "draw";
+    }
+    
+    if (pSelect == "rock" && cSelect == "paper" || pSelect == "paper" && cSelect == "scissors" || pSelect == "scissors" && cSelect == "paper"){
+        computerScore++;
+        roundStatus = "computer";
+    }
+    
+    if (pSelect == "paper" && cSelect == "rock" || pSelect == "rock" && cSelect == "scissors" || pSelect == "scissors" && cSelect == "paper"){
+        playerScore++;
+        roundStatus = "player";
+    }
+}
+
+function updateScore() {
+    winner = roundStatus.toLowerCase();
+    player.textContent = `Player: ${playerScore}`;
+    computer.textContent = `Computer: ${computerScore}`;
+    
+    if(winner == "computer") {
+        winnerStatus.textContent = "You lose!";
+    } else if (winner == "player"){
+        winnerStatus.textContent = "You win!";
+    } else {
+        winnerStatus.textContent = "Draw!";
+    }
+
+
+}
+
+
+// refactor
+rockButton.addEventListener("click", (e) =>{
+    playerWordSelection = e.target.textContent;
+    ComputerWordSelection = getComputerChoice();
+
+    if (isGameFinished()) {
+        return;
+    }
+    playRound(playerWordSelection, ComputerWordSelection);
+    updateScore();
+    console.log(playerScore);
+    console.log(computerScore);
+    console.log(roundStatus);
+});
+
+paperButton.addEventListener("click", (e) =>{
+    playerWordSelection = e.target.textContent;
+    ComputerWordSelection = getComputerChoice();
+
+    if (isGameFinished()) {
+        return;
+    }
+    playRound(playerWordSelection, ComputerWordSelection);
+    updateScore();
+    console.log(playerScore);
+    console.log(computerScore);
+    console.log(roundStatus);
+});
+
+scissorsButton.addEventListener("click", (e) =>{
+    playerWordSelection = e.target.textContent;
+    ComputerWordSelection = getComputerChoice();
+
+    if (isGameFinished()) {
+        return;
+    }
+    playRound(playerWordSelection, ComputerWordSelection);
+    updateScore();
+    console.log(playerScore);
+    console.log(computerScore);
+    console.log(roundStatus);
+});
+
+
